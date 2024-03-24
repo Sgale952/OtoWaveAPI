0c1d024dbe47ef7258c0ef4df2a7d8b0ad99a2b4
KoSmAs <eremingleb6@gmail.com>
2024-03-24 13:49:41 +0700

    Сделал метод add в PlaylistApi

--- src/main/java/github/otowave/otoplaylists/PlaylistApi.java
+++ src/main/java/github/otowave/otoplaylists/PlaylistApi.java
@@ -1,11 +1,41 @@
 package github.otowave.otoplaylists;
+import java.sql.*;
 
+import com.google.gson.Gson;
+import github.otowave.otomusic.MusicApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 
+import static github.otowave.api.DatabaseManager.getConnection;
+
 public class PlaylistApi {
+    private static final Logger logger = LoggerFactory.getLogger(MusicApi.class);
+    private static final Gson gson = new Gson();
+
 
     public static String add(Request req, Response res) {
+        int palylistID = 0;
+        try(Connection conn = getConnection())
+        {
+            String sql =  "INSERT INTO playlists (title,creator, official, public)"+
+                    "VALUES(?, ?, ?, ?)";
+            PreparedStatement stmt = conn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1,PlayListData.title());
+            stmt.setInt(2, PlayListData.creator());
+            stmt.setInt(3,PlayListData.official());
+            stmt.setInt(3,PlayListData.piblic());
+            stmt.executeUpdate();
+            ResultSet generatedKeys = stmt.getGeneratedKeys();
+            palylistID = generatedKeys.getInt(1);
+            res.status(201);
+        }
+        catch (SQLException e)
+        {
+            logger.error("Error in PlaylistApi.add", e);
+            res.status(500);
+        }
         return "";
     }
 
@@ -25,3 +55,4 @@ public class PlaylistApi {
         return "";
     }
 }
+record PlayListData(int official, String title, int creator,int piblic) {}
