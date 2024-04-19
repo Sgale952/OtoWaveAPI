34e9d119235447cdbb8974e8d943cd8a7659ffbc
Sgale <97342601+Sgale952@users.noreply.github.com>
2024-04-19 15:11:15 +0700

    Added Spring to dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,13 @@ version = "0.82"
 
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.2.4"
+    id("io.spring.dependency-management") version "1.1.4"
     id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
@@ -11,23 +17,24 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.mariadb.jdbc:mariadb-java-client:3.3.3")
     implementation("com.zaxxer:HikariCP:5.1.0")
-    implementation("org.zoomba-lang:spark-core:3.0")
     implementation("com.google.code.gson:gson:2.10.1")
 
     implementation("net.bramp.ffmpeg:ffmpeg:0.8.0")
     implementation("com.github.gotson:webp-imageio:0.2.2")
 
-    implementation("org.slf4j:slf4j-api:2.0.9")
-    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation("org.slf4j:slf4j-api:2.0.12")
+    implementation("org.slf4j:slf4j-simple:2.0.13")
 
-    implementation ("io.jsonwebtoken:jjwt-api:0.11.2")
-    runtimeOnly ("io.jsonwebtoken:jjwt-impl:0.11.2")
-    runtimeOnly ("io.jsonwebtoken:jjwt-jackson:0.11.2")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
 }
 
 tasks.jar {
@@ -36,6 +43,6 @@ tasks.jar {
     }
 }
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
