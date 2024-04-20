b6f01cafb237f7de5c3f57f9a3e2a8c28441bfe3
KoSmAs <eremingleb6@gmail.com>
2024-04-20 22:43:11 +0700

    Исправил небольшую ошибку

--- src/main/java/DataSourceConfig.java
+++ src/main/java/DataSourceConfig.java
@@ -10,10 +10,10 @@ public class DataSourceConfig {
     @Bean
     public DataSource getDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-        dataSourceBuilder.url("jdbc:mysql://localhost:3306/mydatabase");
-        dataSourceBuilder.username("db_username");
-        dataSourceBuilder.password("db_password");
+        dataSourceBuilder.driverClassName("org.mariadb.jdbc.Driver");
+        dataSourceBuilder.url("jdbc:mariadb://172.24.80.146:3306/otowave");
+        dataSourceBuilder.username("otoadm");
+        dataSourceBuilder.password("b5T85(CwL#:#");
         dataSourceBuilder.type(HikariDataSource.class);
         return dataSourceBuilder.build();
     }
