fdbc37a7c14d4eceaa3a480383e1994b8dfaa403
Sgale <97342601+Sgale952@users.noreply.github.com>
2024-06-02 09:15:13 +0700

    Simplify GenresDailyMaker

--- src/main/java/github/otowave/api/routes/music/services/faces/MusicDailyFaceMaker.java
+++ src/main/java/github/otowave/api/routes/music/services/faces/MusicDailyFaceMaker.java
@@ -6,6 +6,7 @@ import github.otowave.api.routes.music.repositories.MusicMetaRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+
 @Service
 public class MusicDailyFaceMaker {
     @Autowired
