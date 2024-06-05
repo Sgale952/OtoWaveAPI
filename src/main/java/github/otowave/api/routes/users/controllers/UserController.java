b2f25c015453ee4f2ca563e17c8ac383fbafeb1b
Sgale <97342601+Sgale952@users.noreply.github.com>
2024-06-05 07:48:21 +0700

    Realize users /subscribers /subscribed

--- src/main/java/github/otowave/api/routes/users/controllers/UserController.java
+++ src/main/java/github/otowave/api/routes/users/controllers/UserController.java
@@ -1,8 +1,11 @@
 package github.otowave.api.routes.users.controllers;
 
+import github.otowave.api.routes.actions.services.UserUserActions;
+import github.otowave.api.routes.common.models.FaceModel;
 import github.otowave.api.routes.users.services.Accessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -10,6 +13,8 @@ import reactor.core.publisher.Mono;
 public class UserController {
     @Autowired
     private Accessor accessor;
+    @Autowired
+    UserUserActions userUserActions;
 
     @PostMapping("/register") //Temp unsafe realization
     private Mono<Integer> register(@RequestParam String nickname, @RequestParam String email, @RequestParam String password) {
@@ -19,5 +24,15 @@ public class UserController {
     @GetMapping("/login") //Temp unsafe realization
     private Mono<Integer> login(@RequestParam String email, @RequestParam String password) {
         return accessor.login(email, password);
+    }
+
+    @GetMapping("/{userID}/subscribers")
+    private Flux<FaceModel> subscribers(@PathVariable int userID) {
+        return userUserActions.getSubscribers(userID);
+    }
+
+    @GetMapping("/{userID}/subscribed")
+    private Flux<FaceModel> subscribed(@PathVariable int userID) {
+        return userUserActions.getSubscribed(userID);
     }
 }
